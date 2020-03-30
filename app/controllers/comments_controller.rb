afee1e3b963d4464df1d83039fb6c986c328a762
Vivensad <vasyl.grebenik@gmail.com>
2020-03-30 18:02:54 +0300

    finished deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
-	#http_basic_authenticate_with name: "admin", password: "pepega"
+	
 	def create
 		@post = Post.find(params[:post_id])
 		@comments = @post.comments.create(comment_params)
 		redirect_to post_path(@post)
 	end 
+	
+	def destroy
+		http_basic_authenticate_with name: "admin", password: "pepega"
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+    redirect_to posts_path
+  end
 
 	private def comment_params
 		params.require(:comment).permit(:username, :body)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@
 
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :username, presence: true, length: { minimum: 4 }
+  validates :body, presence: true, length: { minimum: 1 }
 end
